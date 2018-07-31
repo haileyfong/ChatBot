52faed14bf71f2d36e42b00e60048de5b5b4529b
haileyfong <haileyfong1997@gmail.com>
2018-07-31 14:39:50 +0800

    Add files via upload
    Skeleton Files

---
+++ publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../jackjr.zip');
+var kuduApi = 'https://jackjr.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$jackjr';
+var password = 'oATd8juil4g8rTfZzmYSmtTXe9E1yvgrBYLWcfxte2HtiZEa7uH1TtwFffiZ';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('jackjr publish');
+  } else {
+    console.error('failed to publish jackjr', err);
+  }
+});
